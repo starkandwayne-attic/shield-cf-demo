6a9a9eade64056a62d521645b7c0d94fa552439a
James Hunt <james@niftylogic.com>
2017-10-17 15:34:16 -0400

    Start down the road of modularity

--- main.go
+++ main.go
@@ -1,96 +1,145 @@
 package main
 
 import (
-  "fmt"
-  "os"
-  "log"
-  "net/http"
-  "encoding/json"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
 
-  "github.com/garyburd/redigo/redis"
-  "github.com/jhunt/vcaptive"
+	//"github.com/garyburd/redigo/redis"
+	"github.com/jhunt/vcaptive"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintf(w, "Hi there, sending this message from main.go func handler!")
+type Data struct {
+	System       string `json:"system"`
+	Summary      string `json:"summary"`
+	Verification string `json:"verification"`
 }
 
+type System interface {
+	Configure(vcaptive.Services) (bool, error)
+	Setup() error
+	Summarize() Data
+}
+
+var Systems map[string]System
+
 func main() {
-  services, err := vcaptive.ParseServices(os.Getenv("VCAP_SERVICES"))
+	if os.Getenv("VCAP_SERVICES") == "" {
+		fmt.Fprintf(os.Stderr, "No $VCAP_SERVICES environment variable found; are you running me under Cloud Foundry?\n")
+		os.Exit(1)
+	}
+	if os.Getenv("PORT") == "" {
+		fmt.Fprintf(os.Stderr, "No $PORT environment variable found; are you running me under Cloud Foundry?\n")
+		os.Exit(1)
+	}
 
-  if err != nil {
-    fmt.Fprintf(os.Stderr, "VCAP_SERVICES: %s\n", err)
-    os.Exit(1)
-  }
+	services, err := vcaptive.ParseServices(os.Getenv("VCAP_SERVICES"))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "VCAP_SERVICES: %s\n", err)
+		os.Exit(1)
+	}
 
-  instance, found := services.Tagged("redis")
-  if !found {
-    fmt.Fprintf(os.Stderr, "VCAP_SERVICES: no 'redis' service found\n")
-    os.Exit(2)
-  }
+	var (
+		name string
+		system System
+		found bool
+	)
+	/* loop over our known Systems and find the first one that matches */
+	found = false
+	for name, system = range Systems {
+		fmt.Fprintf(os.Stdout, "trying to configure the '%s' system...\n", name)
+		ok, err := system.Configure(services)
+		if !ok {
+			continue
+		}
 
-  host, ok := instance.GetString("host")
-  if !ok {
-    fmt.Fprintf(os.Stderr, "VCAP_SERVICES: '%s' service has no 'host' credential\n", instance.Label)
-    os.Exit(3)
-  }
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Unable to configure %s system: %s\n", name, err)
+			os.Exit(1)
+		}
 
-  port, ok := instance.GetUint("port")
-  if !ok {
-    fmt.Fprintf(os.Stderr, "VCAP_SERVICES: '%s' service has no 'port' credential\n", instance.Label)
-    os.Exit(3)
-  }
+		fmt.Fprintf(os.Stdout, "configured the '%s' system successfully, resuming normal startup...\n", name)
+		found = true
+		break
+	}
+	if !found {
+		fmt.Fprintf(os.Stderr, "No suitable services are bound to this instance of the SHIELD demo application; please try binding a marketplace service and restarting this appplication.\n")
+		os.Exit(2)
+	}
 
-  password, ok := instance.Get("password")
-  if !ok {
-    fmt.Fprintf(os.Stderr, "VCAP_SERVICES: '%s' service has no 'password' credential\n", instance.Label)
-    os.Exit(3)
-  }
+	/*
+	instance, found := services.Tagged("redis")
+	if !found {
+		fmt.Fprintf(os.Stderr, "VCAP_SERVICES: no 'redis' service found\n")
+		os.Exit(2)
+	}
 
-  fmt.Printf("Host is %s.  Port is %d.  Password is %s.\n", host, port, password)
-  // ...
+	host, ok := instance.GetString("host")
+	if !ok {
+		fmt.Fprintf(os.Stderr, "VCAP_SERVICES: '%s' service has no 'host' credential\n", instance.Label)
+		os.Exit(3)
+	}
 
-  conn, err := redis.Dial("tcp", fmt.Sprintf("%s:%d", host, port), redis.DialPassword(password.(string)))
-  if err != nil {
-      log.Fatal(err)
-  }
+	port, ok := instance.GetUint("port")
+	if !ok {
+		fmt.Fprintf(os.Stderr, "VCAP_SERVICES: '%s' service has no 'port' credential\n", instance.Label)
+		os.Exit(3)
+	}
 
-  // Importantly, use defer to ensure the connection is always properly
-  // closed before exiting the main() function.
-  defer conn.Close()
+	password, ok := instance.Get("password")
+	if !ok {
+		fmt.Fprintf(os.Stderr, "VCAP_SERVICES: '%s' service has no 'password' credential\n", instance.Label)
+		os.Exit(3)
+	}
 
-  // Send our command across the connection.
-  _, err = conn.Do("APPEND", "key-example", "value-example")
+	fmt.Printf("Host is %s.  Port is %d.  Password is %s.\n", host, port, password)
+	// ...
 
-  // Check the Err field.
-  if err != nil {
-      log.Fatal(err)
-  }
+	conn, err := redis.Dial("tcp", fmt.Sprintf("%s:%d", host, port), redis.DialPassword(password.(string)))
+	if err != nil {
+		log.Fatal(err)
+	}
 
-  fmt.Println("key value: key-example/value-example  added!")
+	// Importantly, use defer to ensure the connection is always properly
+	// closed before exiting the main() function.
+	defer conn.Close()
 
-  type StructData struct {
-    System string `json:"system"`
-    Summary string `json:"summary"`
-	Verification string `json:"verification"`
-  }
+	// Send our command across the connection.
+	_, err = conn.Do("APPEND", "key-example", "value-example")
 
-  data := StructData{
-    System: "MySQL",
-    Summary: "# Records: 4,236\n# Tables: 15\n",
-    Verification: "c76ecc173eb3a",
-  }
+	// Check the Err field.
+	if err != nil {
+		log.Fatal(err)
+	}
 
-  jData, err:= json.Marshal(data)
-  if err != nil {
-    fmt.Println("error:", err)
-  }
+	fmt.Println("key value: key-example/value-example  added!")
+	*/
 
-  http.HandleFunc("/data", func (w http.ResponseWriter, r *http.Request) {
-    w.Header().Set("Content-Type", "application/json")
-    w.Write(jData)
-  } )
+	fmt.Fprintf(os.Stdout, "setting up data for the %s system...\n", name)
+	if err := system.Setup(); err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to set up the %s system: %s\n", name, err)
+		os.Exit(3)
+	}
 
-  http.Handle("/", http.FileServer(http.Dir("htdocs")))
-  http.ListenAndServe(fmt.Sprintf(":%s", os.Getenv("PORT")), nil)
+	fmt.Fprintf(os.Stdout, "setting up the /data API handler...\n")
+	http.HandleFunc("/data", func(w http.ResponseWriter, r *http.Request) {
+		summary := system.Summarize()
+		b, err := json.Marshal(summary)
+		if err != nil {
+			w.WriteHeader(500)
+			fmt.Fprintf(w, "an internal error has occurred")
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(200)
+		fmt.Fprintf(w, "%s\n", string(b))
+	})
+
+	fmt.Fprintf(os.Stdout, "setting up the / asset handler...\n")
+	http.Handle("/", http.FileServer(http.Dir("htdocs")))
+
+	fmt.Fprintf(os.Stdout, "listening on :%s\n", os.Getenv("PORT"))
+	http.ListenAndServe(fmt.Sprintf(":%s", os.Getenv("PORT")), nil)
 }
